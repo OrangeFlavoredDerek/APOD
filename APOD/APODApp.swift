159f8d734b3f2779b55e08da792472db65c7024a
Derek <chenjunqi07@icloud.com>
2020-07-30 11:50:04 +0800

    Initial Commit

---
+++ APOD/APODApp.swift
@@ -0,0 +1,17 @@
+//
+//  APODApp.swift
+//  APOD
+//
+//  Created by Derek Chan on 2020/7/30.
+//
+
+import SwiftUI
+
+@main
+struct APODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APOD/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  APOD
+//
+//  Created by Derek Chan on 2020/7/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
